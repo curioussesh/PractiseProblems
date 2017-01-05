397e46dde9c85687767b8df83320bd58128b6766
curioussesh <seshadri.skv@gmail.com>
2017-01-05 15:00:09 -0500

    code update

--- HR_Pairs/HR_Pairs/Program.cs
+++ HR_Pairs/HR_Pairs/Program.cs
@@ -4,54 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HR_Pairs
+namespace HR_Practice
 {
-    // https://www.hackerrank.com/challenges/pairs
+    // https://www.hackerrank.com/challenges/connected-cell-in-a-grid
     class Program
     {
 
-        static string line1 = "5 2";
-        static string line2 = "1 5 3 4 2";
-        /* Head ends here */
-        static int pairs(int[] a, int k)
+        static string[] inputCmdStrings = {
+            "4",
+            "4",
+            "1 1 0 0",
+            "0 1 1 0",
+            "0 0 1 0",
+            "1 0 0 0"
+        };
+
+        static int[,] intArray = null;
+
+        static int inputCmdStringsCount = -1;
+        static int maxCount = 0;
+
+        static string GetCmdLineString()
         {
-            int count = 0;
-            Dictionary<int, bool> dictionary = new Dictionary<int, bool>();
-            foreach(int i in a)
-            {
-                dictionary.Add(i, true);
-                if (dictionary.ContainsKey(i + k))
-                {
-                    count++;
-                }
-                if (dictionary.ContainsKey(i - k))
-                {
-                    count++;
-                }
-            }
-            return count;
+            return inputCmdStrings[++inputCmdStringsCount];
         }
-        /* Tail starts here */
+
+        static void connectedcellmaxcount(int rowInd, int columnInd, int currentCount)
+        {
+            if(rowInd < 0 || rowInd >= intArray.GetLength(0) || columnInd < 0 || columnInd >= intArray.GetLength(1))
+            {
+                return;
+            }
+
+            if(intArray[rowInd, columnInd] == 1)
+            {
+                intArray[rowInd, columnInd] = ++currentCount;
+                if (intArray[rowInd, columnInd] >= maxCount) { maxCount = intArray[rowInd, columnInd]; }
+                connectedcellmaxcount(rowInd - 1, columnInd, currentCount);
+                connectedcellmaxcount(rowInd + 1, columnInd, currentCount);
+                connectedcellmaxcount(rowInd, columnInd - 1, currentCount);
+                connectedcellmaxcount(rowInd, columnInd + 1, currentCount);
+
+            }
+            return;
+            
+        }
+       
         static void Main(String[] args)
         {
-            int res;
 
-            String line = line1;
-            String[] line_split = line.Split(' ');
-            int _a_size = Convert.ToInt32(line_split[0]);
-            int _k = Convert.ToInt32(line_split[1]);
-            int[] _a = new int[_a_size];
-            int _a_item;
-            String move = line2;
-            String[] move_split = move.Split(' ');
-            for (int _a_i = 0; _a_i < move_split.Length; _a_i++)
+            int rows = Convert.ToInt32(GetCmdLineString());
+            int columns = Convert.ToInt32(GetCmdLineString());
+            intArray = new int[rows,columns];
+            string line = "";
+            string[] line_split = null;
+
+            for (int i = 0; i < rows; i++)
             {
-                _a_item = Convert.ToInt32(move_split[_a_i]);
-                _a[_a_i] = _a_item;
+                line = GetCmdLineString();
+                line_split = line.Split(' ');
+                for (int j = 0; j < columns; j++)
+                {
+                    intArray[i, j] = Convert.ToInt32(line_split[j]);
+                }
             }
 
-            res = pairs(_a, _k);
-            Console.WriteLine(res);
+            connectedcellmaxcount(0, 0, 0);
+            Console.WriteLine(maxCount);
             Console.ReadLine();
 
         }
--- HR_Pairs/HR_Pairs/Properties/AssemblyInfo.cs
+++ HR_Pairs/HR_Pairs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HR_Pairs")]
+[assembly: AssemblyTitle("HR_Practice")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HR_Pairs")]
+[assembly: AssemblyProduct("HR_Practice")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
